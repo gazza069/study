817a02dab6a5029be7b38c553875f5bcc48d8035
s.takasaki <s.takasaki@nttcoms.com>
2019-02-19 12:24:38 +0900

    初回コミット

---
+++ chapter2/s08/02/main.js
@@ -0,0 +1,13 @@
+
+var app = new Vue({
+    el: '#app',
+    data: {
+      count: 0
+    },
+    methods: {
+      // ボタンをクリックしたときのハンドラ
+      increment: function () {
+        this.count += 1 // 処理は再代入するだけでOK！
+      }
+    }
+})
