173965f7c4a3aa00a37ac8b40583762289e78bff
s.takasaki <s.takasaki@nttcoms.com>
2019-03-12 12:55:13 +0900

    一旦コミット

---
+++ ch4/mstdn-cli/src/styles.js
@@ -0,0 +1,47 @@
+import { Z_FIXED } from "zlib";
+
+const mainColor = '#89C9FA'
+export const styles = {
+    title: {
+        borderBottom: '1px solid silver',
+        backgroundColor: mainColor,
+        color: 'white',
+        fontSize: '1em',
+        padding: 4
+    },
+    editor: {
+        width 600 - 24,
+        padding: 4,
+        font: '1em',
+        padiing: 4
+    },
+    editor: {
+        width: 600 -24,
+        padding: 4,
+        font: '1em',
+        backgroundColor: '#F0F0FF'
+    },
+    editorPad: {
+        position: Z_FIXED,
+        top: 0,
+        width: 600 - 16,
+        height: 120,
+        backgroundColor: 'white'
+    },
+    content: {
+        margin: 8,
+        borderBottom: '1px solid silver'
+    },
+    avatar: {
+        float: 'left',
+        width: 120
+    },
+    ctext: {
+        float: 'left',
+        width: 430,
+        padding: 8
+    },
+    reblog: {
+        backgroundColor: mainColor
+    }
+}
