8f9f904dc201a87eccf31b9231f19d739a23fa67
s.takasaki <s.takasaki@nttcoms.com>
2019-03-29 18:01:57 +0900

    190329分コミット

--- CHAPTER3/13/フォーム入力の取得/main.js
+++ CHAPTER3/13/フォーム入力の取得/main.js
@@ -1,8 +1,12 @@
 var app = new Vue({
     el: '#app',
+    data: {
+        message: 'Hello Vue.js',
+    },
     methods: {
-        handleClick: function() {
-            alert('クリックしたよ')
+        handleInput: function(event) {
+            // 代入前に何か処理を行う
+            this.message = event.target.value
         }
     }
 })
---
+++ CHAPTER3/14/チェックボックス/main.js
@@ -0,0 +1,7 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        val: true,
+        val2: true
+    }
+})
