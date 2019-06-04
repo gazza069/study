01a3b15dd7ec05a93018f7c3493b0a07355516f6
gazza069 <s.takasaki@matadorweb.com>
2019-06-04 10:32:04 +0900

    スライド挙動完成

--- api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// Content-TypeをJSONに指定する
-header('Content-Type: application/json');
-
-// 「200 OK」 で {"data":"24歳、学生です"} のように返す
-$data = "{$_POST['value']}歳です";
-echo json_encode(compact('data'));
--- js/common.js
+++ js/common.js
@@ -1,12 +1,23 @@
 $(function () {
+    var $slide = $('.slide');
+
+    $slide.slick({
+        infinite: false,
+        dots: true
+    });
+
+    $slide.slick('slickFilter', function(index){
+        if (index < 1){
+            return $(this).eq(index);
+        }
+    });
+
     $('.p-post-follow__radio01 input').on('change', function() {
         var radioVal = $('.p-post-follow__radio01 input:checked').val();
         var hostUrl= 'post.json';
         var JSONdata = {
             question01: radioVal   
         };
-        
-        $('.p-post-follow__radio02').removeClass('_is_active');
 
         $.ajax({
             url: hostUrl,
@@ -16,22 +27,27 @@ $(function () {
             timeout:1000,
             success : function(data) {
                 alert("success");
-                $('.p-post-follow__radio02').addClass('_is_active');
+                $slide.slick('slickUnfilter');
+                $slide.slick('slickFilter', function(index){
+                    if (index < 2){
+                        return $(this).eq(index);
+                    }
+                });
+                $slide.slick('slickNext');
             },
             error : function(data) {
                 alert("error");
             }
         });
        
-    })
-    $('.p-post-follow__radio02 input').on('change', function() {
+    });
+
+    $(document).on("change", ".p-post-follow__radio02 input", function(){
         var radioVal = $('.p-post-follow__radio02 input:checked').val();
         var JSONdata = {
             question02: radioVal   
         };
         var hostUrl= 'post.json';
-
-        $('.p-post-follow__radio03').removeClass('_is_active');
 
         $.ajax({
             url: hostUrl,
@@ -41,14 +57,16 @@ $(function () {
             timeout:1000,
             success : function(data) {
                 alert("success");
-                $('.p-post-follow__radio03').addClass('_is_active');
+                $slide.slick('slickUnfilter');
+                $slide.slick('slickNext');
             },
             error : function(data) {
                 alert("error");
             }
         });
-    })
-    $('.p-post-follow__radio03 input').on('change', function() {
+    });
+
+    $(document).on("change", ".p-post-follow__radio03 input", function(){
         var radioVal = $('.p-post-follow__radio03 input:checked').val();
         var JSONdata = {
             question03: radioVal   
@@ -63,10 +81,12 @@ $(function () {
             timeout:1000,
             success : function(data) {
                 alert("success");
+                $slide.removeClass('_is_active');
+                $('.p-post-follow__complete').addClass('_is_active');
             },
             error : function(data) {
                 alert("error");
             }
         });
-    })
+    });
 })
