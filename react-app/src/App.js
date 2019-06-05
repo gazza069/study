201a2751476d7c9e437ec267eb8135a8a232bfc5
gazza069 <s.takasaki@matadorweb.com>
2019-06-05 17:56:09 +0900

    0605分コミット

---
+++ react-app/src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+// import Rect from './Rect';
+import './App.css';
+
+class App extends Component {
+  
+  msgStyle = {
+    fontSize:"24pt",
+    color:"#900",
+    margin:"20px 0",
+    padding: "5px",
+    borderBottom:"2px solid #900"
+  }
+  btnStyle = {
+    fontSize:"20pt",
+    padding:"0 10px"
+  }
+
+  constructor(props){
+    super(props);
+    this.state = {
+      msg:'Hello Component.'
+    };
+    let timer = setInterval(()=>{
+      this.setState((state)=>({
+        msg: state.msg + "!"
+      }));
+    }, 10000)
+  }
+
+  render(){
+    return <div>
+      <h1>React</h1>
+      <p style={this.msgStyle}>{this.state.msg}</p>
+      <p style={this.msgStyle}>{this.props.msg}</p>
+    </div>
+  }
+}
+
+export default App;
