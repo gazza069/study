e3a64d025b537f18e58298a3855fd407ed304125
gazza069 <s.takasaki@matadorweb.com>
2019-07-02 09:53:34 +0900

    一旦コミット

---
+++ Chapter-5/nuxt_app/pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <section class="container">
+    <h1>{{title}}</h1>
+    <p>{{$store.state.message}}</p>
+    <hr>
+    <div class="link" @click="$store.dispatch('doit')">
+      <a @click.stop="$store.commit('reset')">
+        clicked: {{ $store.state.counter }}
+      </a>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  data: function(){
+    return {
+      title:'Hello',
+      message: 'this is message'
+    };
+  },
+};
+</script>
+
+<style>
+a {
+  font-size:16pt; 
+}
+.link {
+  background-color: #def;
+  padding: 10px;
+}
+</style>
