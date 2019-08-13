2c0bdc9844948eb77f4ebbd080a55ca336509a79
gazza069 <s.takasaki@matadorweb.com>
2019-08-13 16:57:09 +0900

    190813分コミット

---
+++ ch01/laravelapp/resources/views/hello/index.blade.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+    <title>Hello/index</title>
+    <style>
+    body {font-size:16pt; color:#999; }
+    h1 { font-size:50pt; text-align:right; color:#f6f6f6; margin:-20pt 0 -30pt 0; letter-spacing:-4pt; }
+    </style>
+</head>
+<body>
+    <h1>Blade/Index</h1>
+    <p>&#064;forディレクティブの例</p>
+    <ol>
+    @for($i = 1;$i < 100;$i++)
+    @if ($i % 2 == 1)
+        @continue
+    @elseif ($i <= 10)
+    <li>No, {{$i}}
+    @else
+        @break
+    @endif
+    @endfor
+    </ol>
+</body>
+</html>
