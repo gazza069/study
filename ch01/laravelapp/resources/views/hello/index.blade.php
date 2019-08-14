d8db1f5566d6f40b6fcfd5a94ba92cad3adda9ec
gazza069 <s.takasaki@matadorweb.com>
2019-08-14 17:52:17 +0900

    190814分コミット

--- ch01/laravelapp/resources/views/hello/index.blade.php
+++ ch01/laravelapp/resources/views/hello/index.blade.php
@@ -1,24 +1,21 @@
-<html>
-<head>
-    <title>Hello/index</title>
-    <style>
-    body {font-size:16pt; color:#999; }
-    h1 { font-size:50pt; text-align:right; color:#f6f6f6; margin:-20pt 0 -30pt 0; letter-spacing:-4pt; }
-    </style>
-</head>
-<body>
-    <h1>Blade/Index</h1>
-    <p>&#064;forディレクティブの例</p>
-    <ol>
-    @for($i = 1;$i < 100;$i++)
-    @if ($i % 2 == 1)
-        @continue
-    @elseif ($i <= 10)
-    <li>No, {{$i}}
-    @else
-        @break
-    @endif
-    @endfor
-    </ol>
-</body>
-</html>
+@extends('layouts.helloapp')
+
+@section('title', 'Index')
+
+@section('menubar')
+    @parent
+    インデックスページ
+@endsection
+
+@section('content')
+    <p>ここが本文のコンテンツです</p>
+    <table>
+    @foreach($data as $item)
+    <tr><th>{{$item['name']}}</th><td>{{$item['mail']}}</td></tr>
+    @endforeach
+    </table>
+@endsection
+
+@section('footer')
+copyright 2017 tuyano
+@endsection
